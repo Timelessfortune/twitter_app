fa51b3e2e84c244246418ed558523c23134c3e01
Andrew <timelessfortune@gmail.com>
2014-02-12 15:59:31 -0500

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ TwitterApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # Lower bcrypt's cost function
+  ActiveModel::SecurePassword.min_cost = true
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name "Andrew Huang"
+		email "andrewh@example.com"
+		password "123456"
+		password_confirmation "123456"
+	end
+end
